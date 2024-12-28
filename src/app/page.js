871e40f4a2163a829ac2b4de33e9a328a535a666
Adriz1611 <adrijbhadra@gmail.com>
2024-12-28 13:07:54 +0530

    created nextjs file with magic ui installation

---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+'use client'
+
+import { useTheme } from "next-themes";
+import { MagicCard } from "@/components/ui/magic-card";
+
+// Changed to default export for Next.js page
+export default function Page() {
+  const { theme } = useTheme();
+  return (
+    <div
+      className={
+        "flex h-[500px] w-full flex-col gap-4 lg:h-[250px] lg:flex-row"
+      }
+    >
+      <MagicCard
+        className="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
+        gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+      >
+        Magic
+      </MagicCard>
+      <MagicCard
+        className="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
+        gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+      >
+        Card
+      </MagicCard>
+    </div>
+  );
+}
